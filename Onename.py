b9e600c4a726f4cfccea20d0a360eab3c33cf1da
Justin Chase <jujowned@Gmail.com>
2014-09-10 22:50:07 -0700

    Profile data Creation Functions, Casting, .json fileoutput, test func
    Profile data Creation Functions, Casting, .json fileoutput, test func
    
    Profile data needs to be updated when v3 is used, also it looks like I
    need to do a bit more testing too include all the proofs etc for the
    from input. Need to test a facebook and facebook proof
    
    Casted alot of the returns as String just to remove some of the print
    concatenate issues.
    
    Wrote .json output function
    
    Consolidated the testing to a general function that is commented out,
    was just testing things as I went, should use unit test and test against
    my and another complete profile with proof: twitter, github, facebook,
    etc

--- Onename.py
+++ Onename.py
@@ -2,8 +2,8 @@
 #Description: Simple and Small API for interacting with OneName.io json data
 import json
 import urllib2
-
-#Pre:Given the onename.io USERNAME
+ 
+#Pre: Given the (u/) USERNAME
 #Post: Returns the entire json object using opendig
 def getOpenDigJson(USERNAME):
 	from opendig import ons_resolver #sudo pip install opendig - https://github.com/opennamesystem/opendig
@@ -13,8 +13,8 @@ def getOpenDigJson(USERNAME):
 		return "No data found to return"
 	else:
 		return data
-
-#Pre:Given the onename.io USERNAME
+ 
+#Pre: Given the (u/) USERNAME
 #Post: Returns the entire json object
 def getOneNameJson(USERNAME):
 	URL='https://onename.io/' + str(USERNAME) + '.json'
@@ -24,22 +24,22 @@ def getOneNameJson(USERNAME):
 		return str("HTTPException")
 	else:
 		return data
-
-#Pre:Given the onename.io USERNAME
+ 
+#Pre: Given the (u/) USERNAME
 #Post: Returns .json URL
 def getOneNameJsonURL(USERNAME):
 	URL='https://onename.io/' + str(USERNAME) + '.json'
 #	print URL
-	return URL
-
-#Pre:Given the onename.io USERNAME
+	return str(URL)
+ 
+#Pre: Given the (u/) USERNAME
 #Post: Returns the entire .json file
 def getOneNameURL(USERNAME):
-	URL="https://onename.io/" + USERNAME
+	URL="https://onename.io/" + str(USERNAME)
 #	print URL
-	return URL
-
-#Pre:Given the onename.io/USERNAME.json
+	return str(URL)
+ 
+#Pre: Given the profile data
 #Post: Returns the bitcoin address
 def getBitcoin(JSONTOPARSE):
 	try:
@@ -48,9 +48,9 @@ def getBitcoin(JSONTOPARSE):
 		return "No Bitcoin Address found"
 	else:
 	#	print BITCOIN
-		return BITCOIN
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(BITCOIN)
+ 
+#Pre: Given the profile data
 #Post: Returns the name formatted
 def getRealName(JSONTOPARSE):
 	try:
@@ -59,9 +59,9 @@ def getRealName(JSONTOPARSE):
 		return "No Name found"
 	else:
 	#	print REALNAME
-		return REALNAME
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(REALNAME)
+ 
+#Pre: Given the profile data
 #Post: Returns the PGP Keyserver URL
 def getPGPURL(JSONTOPARSE):
 	try:
@@ -70,9 +70,9 @@ def getPGPURL(JSONTOPARSE):
 		return "No PGP key found"
 	else:
 #		print PGPURL
-		return PGPURL
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(PGPURL)
+ 
+#Pre: Given the profile data
 #Post: Returns the PGP Fingerprint
 def getPGPFingerPrint(JSONTOPARSE):
 	try:
@@ -81,9 +81,9 @@ def getPGPFingerPrint(JSONTOPARSE):
 		return "No PGP key found"
 	else:
 	#	print FINGERPRINT
-		return FINGERPRINT
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(FINGERPRINT)
+ 
+#Pre: Given the profile data
 #Post: Returns the BIO
 def getBio(JSONTOPARSE):
 	try:
@@ -92,9 +92,9 @@ def getBio(JSONTOPARSE):
 		return "No Bio found"
 	else:
 	#	print BIO
-		return BIO
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(BIO)
+ 
+#Pre: Given the profile data
 #Post: Returns the website URL
 def getWebsite(JSONTOPARSE):
 	try:
@@ -103,9 +103,9 @@ def getWebsite(JSONTOPARSE):
 		return "No website found."
 	else:
 	#	print WEBSITE
-		return WEBSITE
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(WEBSITE)
+ 
+#Pre: Given the profile data
 #Post: Returns the Location formatted
 def getLocation(JSONTOPARSE):
 	try:
@@ -114,9 +114,9 @@ def getLocation(JSONTOPARSE):
 		return "No Location found"
 	else:
 	#	print LOCATION
-		return LOCATION
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(LOCATION)
+ 
+#Pre: Given the profile data
 #Post: Returns the GITHUB
 def getGithub(JSONTOPARSE):
 	try:
@@ -125,9 +125,9 @@ def getGithub(JSONTOPARSE):
 		return "No Github found"
 	else:
 	#	print GITHUB
-		return GITHUB
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(GITHUB)
+ 
+#Pre: Given the profile data
 #Post: Returns the GITHUB URL
 def getGithubURL(JSONTOPARSE):
 	try:
@@ -136,9 +136,9 @@ def getGithubURL(JSONTOPARSE):
 		return "No Github found"
 	else:
 	#	print GITHUB
-		return "https://github.com/"+GITHUB
-
-#Pre:Given the onename.io/USERNAME.json
+		return "https://github.com/"+ str(GITHUB)
+ 
+#Pre: Given the profile data
 #Post: Returns the twitter @handle
 def getTwitter(JSONTOPARSE):
 	try:
@@ -147,9 +147,9 @@ def getTwitter(JSONTOPARSE):
 		return "No Twitter found"
 	else:
 	#	print TWITTER
-		return TWITTER
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(TWITTER)
+ 
+#Pre: Given the profile data
 #Post: Returns the whole twitter URL
 def getTwitterURL(JSONTOPARSE):
 	try:
@@ -158,9 +158,9 @@ def getTwitterURL(JSONTOPARSE):
 		return "No Twitter found"
 	else:
 	#	print TWITTER
-		return "http://twitter/" + TWITTER
-
-#Pre:Given the onename.io/USERNAME.json
+		return "http://twitter/" + str(TWITTER)
+ 
+#Pre: Given the profile data
 #Post: Returns the Cover URL
 def getCover(JSONTOPARSE):
 	try:
@@ -168,9 +168,9 @@ def getCover(JSONTOPARSE):
 	except KeyError:
 		return "No Cover image found"
 	else:
-		return COVER
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(COVER)
+ 
+#Pre: Given the profile data
 #Post: Returns the Avatar URL
 def getAvatar(JSONTOPARSE):
 	try:
@@ -179,9 +179,9 @@ def getAvatar(JSONTOPARSE):
 		return "No Avatar image found"
 	else:
 		#	print AVATAR
-		return AVATAR
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(AVATAR)
+ 
+#Pre: Given the profile data
 #Post: Returns the Graph URL
 def getGraph(JSONTOPARSE):
 	try:
@@ -190,9 +190,9 @@ def getGraph(JSONTOPARSE):
 		return "No Graph image found"
 	else:
 	#	print GRAPH
-		return GRAPH
-
-#Pre:Given the onename.io/USERNAME.json
+		return str(GRAPH)
+ 
+#Pre: Given the profile data
 #Post: Returns the version
 def getVersion(JSONTOPARSE):
 	try:
@@ -201,9 +201,9 @@ def getVersion(JSONTOPARSE):
 		return "No Version found"
 	else:
 	#	print VERSION
-		return VERSION
-
-#Pre:Given the onename.io/USERNAME in a text file
+		return str(VERSION)
+ 
+#Pre: Given the usernames (u/) in a text file
 #Post: Returns the version associated Bitcoin address and username in a csv format
 def getAllBTCAddrsOpenDig(FILEPATHNAME,OUTPUTFILEPATH):
 	f = open(FILEPATHNAME, 'r')
@@ -214,8 +214,16 @@ def getAllBTCAddrsOpenDig(FILEPATHNAME,OUTPUTFILEPATH):
 			BTCADDR=getBitcoin(data)
 			print line.rstrip('\r\n')+","+str(BTCADDR)+"\n"
 			o.write(line.rstrip('\r\n')+","+str(BTCADDR)+"\n")
+	o.close()
+	f.close()
 
-#Pre:Given the onename.io/USERNAME in a text file
+#Pre: Given the Output file path and name and the Json to write
+#Post: Writes out the JSON data to the given Output file path
+def writeOutJson(OUTPUTFILEPATH, JSONTOWRITE):
+	with open(OUTPUTFILEPATH, 'w') as outfile:
+		json.dump(JSONTOWRITE, outfile)
+
+#Pre: Given the usernames (u/) in a text file
 #Post: Returns the version associated Bitcoin address and username in a csv format
 def getAllBTCAddrs(FILEPATHNAME,OUTPUTFILEPATH):
 	f = open(FILEPATHNAME, 'r')
@@ -226,8 +234,10 @@ def getAllBTCAddrs(FILEPATHNAME,OUTPUTFILEPATH):
 			BTCADDR=getBitcoin(data)
 			print line.rstrip('\r\n')+","+str(BTCADDR)+"\n"
 			o.write(line.rstrip('\r\n')+","+str(BTCADDR)+"\n")
-
-#Pre:Given the onename.io/USERNAME
+	o.close()
+	f.close()
+ 
+#Pre: Given the username (u/)
 #Post: Prints all associated information related to that user using the onename.io .json files
 def listAllJson(USERNAME):
 	data=getOneNameJson(USERNAME)
@@ -253,7 +263,26 @@ def listAllJson(USERNAME):
 	else:
 		print USERNAME + " not found or this is reserved."
 
-#Pre:Given the onename.io/USERNAME
+#Pre: Given the Profile data prints it out
+#Post: Prints all associated information related to that user using the Profile data passed in as parameter
+def listAllJsonData(data):
+	print "Bitcoin Address: " + getBitcoin(data)
+	print "Real Name: " + getRealName(data)
+	print "PGP Fingerprint: " + getPGPFingerPrint(data)
+	print "PGP Keyserver URL: " + getPGPURL(data)
+	print "Cover image URL: " + getCover(data)
+	print "Avatar image URL: " + getAvatar(data)
+	print "Graph image URL: " + getGraph(data)
+	print "Twitter: " + getTwitter(data)
+	print "Twitter URL: " + getTwitterURL(data)
+	print "Github: " + getGithub(data)
+	print "Github URL: " + getGithubURL(data)
+	print "Location: " + getLocation(data)
+	print "Website: " + getWebsite(data)
+	print "Bio: " + getBio(data)
+	print "Version: " + getVersion(data)
+ 
+#Pre: Given the username (u/)
 #Post: Prints all associated information related to that user using the onename.io .json files
 def listAllOpendigJson(USERNAME):
 	data=getOpenDigJson(USERNAME)
@@ -276,11 +305,131 @@ def listAllOpendigJson(USERNAME):
 	print "Bio: " + getBio(data)
 	print "OneName.io URL: " + getOneNameURL(USERNAME)
 	print "Version: " + getVersion(data)
+	
+#Pre: Given Profile v02 Information creates a Profile
+#Post: Returns a Profile with the given Information, if something is not given NoneType is passed in
+def format_profile_v02(name=None, location=None, bio=None, website=None, avatar_url=None, cover_url=None, twitter_username=None,twitter_proof=None, facebook_username=None,facebook_proof=None, linkedin_username=None,linkedin_proof=None, github_username=None, github_proof=None, bitcoin_address=None, pgp_fingerprint=None, pgp_url=None):
+	profile ={
+		"avatar": 
+		{
+			"url": ""
+		},
+		"website": "", 
+		"bio": "", 
+		"cover": 
+		{
+			"url": ""
+		}, 
+		"twitter": 
+		{
+			"proof": 
+			{
+				"url": ""
+			}, 
+			"username": ""
+		},
+		"facebook": 
+		{
+			"proof": 
+			{
+				"url": ""
+			}, 
+			"username": ""
+		},
+		"linkedin": 
+		{
+			"proof": 
+			{
+				"url": ""
+			}, 
+			"username": ""
+		}, 
+		"graph": 
+		{
+			"url": ""
+		}, 
+		"pgp": 
+		{
+			"fingerprint": "",
+			"url": ""
+		}, 
+		"v": "0.2", 
+		"location": 
+		{
+			"formatted": ""
+		}, 
+		"github": 
+		{
+			"proof": 
+			{
+				"url": ""
+			}, 
+			"username": ""
+		}, 
+		"name": 
+		{
+			"formatted": ""
+		}, 
+		"bitcoin": 
+		{
+			"address": ""
+		}
+	}
+	profile['v'] = '0.2'
+	profile['name']['formatted'] = name
+	profile['location']['formatted'] = location
+	profile['bio'] = bio
+	profile['website'] = website
+	profile['avatar'] = {"url": avatar_url}
+	profile['cover'] = {"url": cover_url}
+	profile['bitcoin'] = {"address": bitcoin_address}
+	profile['twitter'] = {"username": twitter_username}
+	profile['twitter']['proof']={"url": twitter_proof}
+	profile['facebook'] = {"username": facebook_username}
+	profile['facebook']['proof']={"url": facebook_proof}
+	profile['linkedin'] = {"username": linkedin_username}
+	profile['linkedin']['proof']={"url": linkedin_proof}
+	profile['github'] = {"username": github_username}
+	profile['github']['proof']={"url": github_proof}
+	profile['pgp']['fingerprint'] = pgp_fingerprint
+	profile['pgp']['url'] = pgp_url
+	return profile
+ 
+#Pre: Given Profile v02 Information from Input (Taken from OpenSpecs)
+#Post: Returns a profile with v02 no validation for Username ownership
+def format_profile_from_user_input_v02():
+	profile = {}
+	input_items = [
+		{'name': 'name', 'prompt': 'Name: '},
+		{'name': 'location', 'prompt': 'Location: '},
+		{'name': 'bio', 'prompt': 'Bio: '},
+		{'name': 'website', 'prompt': 'Website: '},
+		{'name': 'twitter_username', 'prompt': 'Twitter username: '},
+		{'name': 'github_username', 'prompt': 'Github username: '},
+		{'name': 'avatar_url', 'prompt': 'Avatar url: '},
+		{'name': 'cover_url', 'prompt': 'Cover url: '},
+		{'name': 'bitcoin_address', 'prompt': 'Bitcoin address: '},
+		{'name': 'pgp_fingerprint', 'prompt': 'PGP Key Fingerprint: '},
+		{'name': 'pgp_url', 'prompt': 'URL of hosted PGP Public Key: '},
+	]
+	for input_item in input_items:
+		profile[input_item['name']] = raw_input(input_item['prompt'])
+	return profile
 
-###########################
-#For Testing Purposes
-#getAllBTCAddrs("newlist", "output.csv")
-#NAME='juju'
-#listAllJson(NAME)
-#listAllOpendigJson(NAME)
-###########################
+#Pre: Testing Function
+#Post: Testing Function
+#def testt():
+	#getAllBTCAddrs("newlist", "output.csv")
+	#NAME='juju'
+	#profile=getOneNameJson(NAME)
+	#print type(profile)
+	#print profile["bitcoin"]["address"]
+	#listAllJson(NAME)
+	#listAllOpendigJson(NAME)
+	#profile=format_profile_from_user_input_v02()
+	#print profile
+	#profile=format_profile_v02(name='FirstName LastName', location='Location', bio=None, website=None, avatar_url=None, cover_url=None, twitter_username=None,twitter_proof=None, facebook_username=None,facebook_proof=None, linkedin_username=None,linkedin_proof=None, github_username=None, github_proof=None, bitcoin_address=None, pgp_fingerprint=None, pgp_url=None)
+	#print profile
+	#listAllJsonData(profile)
+	#writeOutJson(NAME + '.json', profile)
+#testt()
